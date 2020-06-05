086af9eeb40518105d1aee17c15f39babafb8998
vinhandyle <vinhandyle@gmail.com>
2020-06-04 22:25:43 -0700

    Unity Update
    Lots of changes, but not really...

---
+++ CodeGamesSummer2020/Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool midair = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Press "a" to move left
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Time.deltaTime * -5, 0, 0);
+        }
+
+        // Press "d" to move right
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Time.deltaTime * 5, 0, 0);
+        }
+
+        // Press "space" to jump
+        if (Input.GetKey("space") && !midair)
+        {
+            transform.Translate(0, Time.deltaTime * 9, 0);
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            midair = true;
+        }
+
+        if(Collider2D.IsTouching("Floor"))
+    }
+}
+
